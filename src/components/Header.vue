f35c2bc1640a2620405c8320386326feb25d563d
Qasim223 <41677739+Qasim223@users.noreply.github.com>
2022-02-11 13:34:59 +0000

    Vue Project start

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button />
+    </header>
+</template>
+
+<script>
+
+    import Button from './components/Button.vue';
+
+    export default {
+        name: 'header',
+        props: {
+            title: String,
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+      display: flex;  
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 20px;
+    }
+</style>
